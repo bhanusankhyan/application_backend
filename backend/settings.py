7ad1bee6249563fcec7a1e88e9424fa51888b2e1
unknown <bhanusankhyan77@gmail.com>
2024-12-26 13:33:36 +0530

    database_setup

--- backend/settings.py
+++ backend/settings.py
@@ -96,8 +96,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        # default='postgresql://applications_db_z6y0_user:Rnf4JLiTA8Cl6PxMMJl6TCyc8GmO0wIF@dpg-ctldf00gph6c739kbv10-a.oregon-postgres.render.com/applications_db_z6y0',
-        default='postgresql://applications_db_z6y0_user:Rnf4JLiTA8Cl6PxMMJl6TCyc8GmO0wIF@dpg-ctldf00gph6c739kbv10-a/applications_db_z6y0',
+        default='postgresql://applications_db_z6y0_user:Rnf4JLiTA8Cl6PxMMJl6TCyc8GmO0wIF@dpg-ctldf00gph6c739kbv10-a.oregon-postgres.render.com/applications_db_z6y0',
+        # default='postgresql://applications_db_z6y0_user:Rnf4JLiTA8Cl6PxMMJl6TCyc8GmO0wIF@dpg-ctldf00gph6c739kbv10-a/applications_db_z6y0',
         # default = 'postgresql://postgres:postgres@localhost:5432/app_integrations',
         conn_max_age=600
     )
